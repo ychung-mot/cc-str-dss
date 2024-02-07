3b79cdea29141c70c02e2d680b63e32d23d52de1
ychung-mot <young-jin.chung@dxcas.com>
2024-02-07 09:27:42 -0800

    chore: more workflows

---
+++ .conventional-changelog.mjs
@@ -0,0 +1,41 @@
+export default {
+  options: {
+    preset: {
+      name: 'conventionalcommits',
+      types: [
+        {
+          type: 'feat',
+          section: 'Features',
+        },
+        {
+          type: 'fix',
+          section: 'Bug Fixes',
+        },
+        {
+          type: 'docs',
+          section: 'Docs',
+        },
+        {
+          type: 'refactor',
+          section: 'Refactors',
+        },
+        {
+          type: 'chore',
+          hidden: true,
+        },
+        {
+          type: 'style',
+          hidden: true,
+        },
+        {
+          type: 'perf',
+          hidden: true,
+        },
+        {
+          type: 'test',
+          hidden: true,
+        },
+      ],
+    },
+  },
+};
