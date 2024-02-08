ae151a65971bb07ce3db3d41442a1ccd04c50452
ychung-mot <young-jin.chung@dxcas.com>
2024-02-08 10:37:49 -0800

    chore: enable strict csp

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -9,7 +9,7 @@ echo "hit entrypoint..."
 cat >/nginx/nginx.conf <<EOL
 # See https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
 # See https://content-security-policy.com/nonce/
-# add_header content-security-policy "default-src 'self'; style-src 'self' 'nonce-${STYLE_NONCE}'; frame-src loginproxy.gov.bc.ca dev.loginproxy.gov.bc.ca test.loginproxy.gov.bc.ca; connect-src 'self' loginproxy.gov.bc.ca dev.loginproxy.gov.bc.ca test.loginproxy.gov.bc.ca server.arcgisonline.com; img-src 'self' data: server.arcgisonline.com www.w3.org; frame-ancestors https://loginproxy.gov.bc.ca https://dev.loginproxy.gov.bc.ca https://test.loginproxy.gov.bc.ca; object-src 'none'; base-uri 'self'; form-action 'self';";
+add_header content-security-policy "default-src 'self'; style-src 'self' 'nonce-${STYLE_NONCE}'; frame-src loginproxy.gov.bc.ca dev.loginproxy.gov.bc.ca test.loginproxy.gov.bc.ca; connect-src 'self' loginproxy.gov.bc.ca dev.loginproxy.gov.bc.ca test.loginproxy.gov.bc.ca server.arcgisonline.com; img-src 'self' data: server.arcgisonline.com www.w3.org; frame-ancestors https://loginproxy.gov.bc.ca https://dev.loginproxy.gov.bc.ca https://test.loginproxy.gov.bc.ca; object-src 'none'; base-uri 'self'; form-action 'self';";
 
 # add_header content-security-policy "default-src 'self'; style-src 'self' 'unsafe-inline'; frame-src loginproxy.gov.bc.ca dev.loginproxy.gov.bc.ca test.loginproxy.gov.bc.ca; connect-src 'self' loginproxy.gov.bc.ca dev.loginproxy.gov.bc.ca test.loginproxy.gov.bc.ca server.arcgisonline.com; img-src 'self' data: server.arcgisonline.com www.w3.org; frame-ancestors https://loginproxy.gov.bc.ca https://dev.loginproxy.gov.bc.ca https://test.loginproxy.gov.bc.ca; object-src 'none'; base-uri 'self'; form-action 'self';";
 
